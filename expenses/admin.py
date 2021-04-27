ed759c5a428d78ef1b01dfb17d2106d63c351110
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2021-04-27 21:02:54 +0100

    Create Expense model and register model

--- expenses/admin.py
+++ expenses/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from expenses.models import Expense
+
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    pass
--- expenses/models.py
+++ expenses/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+categories = (
+    ('Food', 'Food'),
+    ('Entertainment', 'Entertainment'),
+    ('Bills', 'Bills'),
+    ('Travel', 'Travel')
+)
+
+
+class Expense(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=15, choices=categories)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.amount} - {self.category}"
+
+    @classmethod
+    def search_by_id(cls, id):
+        return cls.objects.get(id=id)
