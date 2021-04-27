c07023ae3f736d111ef0217670b51c1e8553a0e5
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2021-04-27 22:09:11 +0100

    modify Expense model

--- expenses/admin.py
+++ expenses/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-
 from expenses.models import Expense
 
 
 @admin.register(Expense)
 class ExpenseAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'amount', 'currency', 'category', 'updated_at')
+    search_fields = ('category', )
+    list_per_page = 25
+    list_display_links = ('id', 'amount')
--- expenses/models.py
+++ expenses/models.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
-from users.models import User
+
 
 categories = (
     ('Food', 'Food'),
@@ -8,17 +10,28 @@ categories = (
     ('Travel', 'Travel')
 )
 
+currencies = (
+    ('PLN', 'PLN'),
+    ('GBP', 'GBP'),
+)
+
 
 class Expense(models.Model):
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.CharField(max_length=15, choices=categories)
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
+    currency = models.CharField(max_length=3, choices=currencies)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.amount} - {self.category}"
+        return f"{self.amount} {self.currency} - {self.category}"
 
     @classmethod
-    def search_by_id(cls, id):
-        return cls.objects.get(id=id)
+    def get_by_id(cls, id):
+        try:
+            expense = cls.objects.get(id=id)
+        except ObjectDoesNotExist:
+            return None
+        else:
+            return expense
