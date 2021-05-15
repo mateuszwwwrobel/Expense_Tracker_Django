89f94c0648b81f2723e60623655c9c7957dbc554
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2021-05-15 10:51:04 +0100

    Register models to admin

--- expenses/admin.py
+++ expenses/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
-from expenses.models import Expense
+from expenses.models import Expense, Budget
 
 
 @admin.register(Expense)
 class ExpenseAdmin(admin.ModelAdmin):
-    list_display = ('id', 'amount', 'currency', 'category', 'updated_at')
-    search_fields = ('category', )
+    list_display = ('id', 'price', 'category', 'updated_at')
     list_per_page = 25
-    list_display_links = ('id', 'amount')
+    list_display_links = ('id', 'price')
+
+
+@admin.register(Budget)
+class BudgetAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'currency')
+    list_per_page = 25
+    list_display_links = ('id', 'name')
