14511ecabd8fe536011b0048ec6b4ddade998d09
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2021-06-12 12:19:55 +0100

    Add default authentication fro DRF

--- ExpenseTracker/settings.py
+++ ExpenseTracker/settings.py
@@ -142,7 +142,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication'
     ]
 }
-
-
