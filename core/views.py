538b381ae27d42582ad1998b4b220d7556ee7c68
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2021-06-16 19:53:46 +0100

    Add form to get statistics data from db

--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views.generic import TemplateView, View, DetailView
@@ -76,11 +77,45 @@ class CreateBudgetView(LoginRequiredMixin, View):
 
 class BudgetStats(LoginRequiredMixin, View):
 
-    def get(self, request, budget_id):
-        budget = Budget.objects.get(id=budget_id)
+    def get(self, request, pk):
+        budget = Budget.objects.get(id=pk)
 
         context = {
             'budget': budget,
         }
 
-        return render(request, 'logged/budget_stats.html', context)
+        return render(request, 'logged/budget_stats_select.html', context)
+
+
+class ShowStatistics(LoginRequiredMixin, View):
+
+    def get(self, request):
+        budget_id = request.GET['budget']
+        start_date = request.GET['start']
+        end_date = request.GET['end']
+
+        budget = Budget.objects.get(id=budget_id)
+
+        if end_date != '':
+            s_year = int(start_date[:4])
+            s_month = int(start_date[-2:])
+            e_year = int(end_date[:4])
+            e_month = 1 if end_date[-2:] == 12 else int(end_date[-2:])
+
+            expenses = Expense.objects.filter(budget=budget).\
+                filter(created_at__gte=datetime(s_year, s_month, 1)).\
+                filter(created_at__lt=datetime(e_year, e_month + 1, 1))
+        else:
+            # annotate pomoże na categorie rozbic?
+            year = start_date[:4]
+            month = start_date[-2:]
+
+            expenses = Expense.objects.filter(budget=budget).\
+                filter(created_at__year=year).\
+                filter(created_at__month=month)
+
+        context = {
+            'expenses': expenses,
+        }
+
+        return render(request, 'logged/budget_stats_show.html', context)
