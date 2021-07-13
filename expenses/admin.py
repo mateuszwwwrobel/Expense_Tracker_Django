ae07bc0e28b601bc7f837f53f147a835fd4b1327
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2021-07-13 21:40:10 +0100

    Update admin view

--- expenses/admin.py
+++ expenses/admin.py
@@ -4,7 +4,7 @@ from expenses.models import Expense, Budget
 
 @admin.register(Expense)
 class ExpenseAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'budget', 'category', 'updated_at')
+    list_display = ('name', 'price', 'budget', 'category', 'created_at', 'updated_at')
     list_per_page = 25
     list_display_links = ('name', 'price')
 
