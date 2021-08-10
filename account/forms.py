049d8a303047b96e020b5d9e324216a58ff6595e
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2021-08-10 13:16:43 +0100

    Add docs to functions

--- account/forms.py
+++ account/forms.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
+    """Custom signup form with additional fields."""
+
     first_name = forms.CharField(max_length=50, required=False,
                                  help_text='Optional. 50 characters or fewer.')
     last_name = forms.CharField(max_length=50, required=False,
